0b01da4603987a49133231ed6f44a791e2616250
b4ttl3C4t <129575894+b4ttl3C4t@users.noreply.github.com>
2023-10-07 20:55:31 +0800

    Reconstruct the program.

--- limit.c
+++ limit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #define LOWER -10000000000000
@@ -7,13 +8,10 @@
 void limit_to_zero(double (*)(double));
 //The iterative bisection method.
 double iter_bisect_method        (double upper, double lower, double target);
-//The iterative digit-by-digit calculation..
-double iter_digit_calculation    (double upper, double lower, double target);
-//The recursive bisection method.
-double recu_bisect_method        (double upper, double lower, double target);
-//The recursive digit-by-digit calculation.
-double recu_digit_calculation    (double upper, double lower, double target);
 
+double definite_integral_right(double, double, double, double (double));
+double definite_integral_trapezium(double, double, double, double (double));
+double definite_integral_Simpson(double, double, double (double));
 
 double f(double x)
 {
@@ -66,4 +64,66 @@ double iter_bisect_method(double lower_limit, double upper_limit, double target)
     }
     
     return middle;
+}
+
+//Calculating definite integral by right side height.
+double definite_integral_right(double lower, double upper, double partition, double func(double))
+{
+    double summation = 0.0;
+    double height    = 0.0;
+    double width     = (upper - lower) / partition;
+    unsigned int i;
+
+    for(i = 1; i <= partition; ++i)
+    {
+        /*Obtaining the approximate summation of func (integral from lower to upper)
+         *by calculating the height multiple the width step by step.
+         */
+        height = func(lower + i * width);
+        summation += height;
+    }
+
+    //You can move out the multiplication of width by distributive property.
+    summation *= width;
+
+    return summation;
+}
+
+//Calculating definite integral by trapezium.
+double definite_integral_trapezium(double lower, double upper, double partition, double func(double))
+{
+    double summation = 0.0;
+    double height    = 0.0;
+    double width     = (upper - lower) / partition;
+    unsigned int i;
+
+    summation += func(lower) / 2;
+    for(i = 1; i < partition; ++i)
+    {
+        height = func(lower + i * width);
+        summation += height;
+    }
+    summation += func(upper) / 2;
+
+    //You can move out the multiplication of width by distributive property.
+    summation *= width;
+
+    return summation;
+}
+
+/*Calculating definite integral by curse,
+ *and you don't have to get the summation by split and input the partiton.
+ */
+double definite_integral_Simpson(double lower, double upper, double func(double))
+{
+    double summation = 0.0;
+
+    summation += func(lower);
+    summation += 4 * func((lower + upper) / 2);
+    summation += func(upper);
+
+    //You can move out the multiplication of width by distributive property.
+    summation *= (upper - lower) / 6;
+
+    return summation;
 }
